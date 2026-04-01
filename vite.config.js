ddb470cc88a6783fcd61d8c1ca2df5e33eaae769
first--github-project <amd142457@gmail.com>
2026-04-01 09:55:53 +0300

    added base path for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/B13-A6/", // এই লাইনটি আপনার প্রোজেক্টের নাম অনুযায়ী যোগ করা হয়েছে
 });
