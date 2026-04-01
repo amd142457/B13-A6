2e9b4508bff27d430959004f44e3417a0390fa08
first--github-project <amd142457@gmail.com>
2026-04-01 09:03:30 +0300

    fix homepage

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+
+  // এই লাইনটা খুব জরুরি GitHub Pages এর জন্য
+  base: "/B13-A6/", // ← তোমার repository নাম exactly এভাবে দাও (শেষে / সহ)
 });
