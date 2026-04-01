ee18599e39c00d5a245afaa7297f2a0f7c676eae
first--github-project <amd142457@gmail.com>
2026-04-01 10:55:57 +0300

    moved images to public and updated paths

--- src/componants/ModelCard.jsx
+++ src/componants/ModelCard.jsx
@@ -11,46 +11,31 @@ const ModelCard = ({ model, isCart, setCart }) => {
   };
 
   return (
-    <a href="#" className="hover-3d  cursor-pointer  ">
-      <div className="mt-7 ">
-        <div className=" shadow-lg p-4 flex flex-col h-full rounded-xl space-y-3 border border-gray-300 bg-linear-to-br from-[#f5f3ff] via-[#ede9fe] to-[#ddd6fe]">
-          <div className="flex justify-end ">
-            <p className=" btn rounded-full ">{model.tagType}</p>
-          </div>
-          <p className="w-10 h-10 rounded-full border border-gray-400 flex justify-center items-center">
-            {model.icon}
-          </p>
-          <h2 className="font-bold text-xl">{model.name}</h2>
-          <p className="text-[0.90em] text-gray-500">{model.description}</p>
-          <p className="font-bold">${model.price}/month</p>
-          {model.features.map((item, index) => (
-            <div
-              key={index}
-              className="flex gap-1 text-[0.90em] text-gray-500 "
-            >
-              <Check className="w-4 text-green-400 " />
-              {item}
-            </div>
-          ))}
-          <button
-            className="btn w-full btn-primary mt-auto rounded-full"
-            onClick={handlesuscribtion}
-          >
-            {isSubcrived ? "Card to add" : "Buy now"}
-          </button>
+    <div className="mt-7 ">
+      <div className=" shadow-lg p-4 flex flex-col h-full rounded-xl space-y-3 bg-gray-100">
+        <div className="flex justify-end ">
+          <p className=" btn rounded-full ">{model.tagType}</p>
         </div>
+        <p className="w-10 h-10 rounded-full border border-gray-200 flex justify-center items-center">
+          {model.icon}
+        </p>
+        <h2 className="font-bold text-xl">{model.name}</h2>
+        <p className="text-[0.90em] text-gray-500">{model.description}</p>
+        <p className="font-bold">${model.price}/month</p>
+        {model.features.map((item, index) => (
+          <div key={index} className="flex gap-1 text-[0.90em] text-gray-500 ">
+            <Check className="w-4 text-green-400 " />
+            {item}
+          </div>
+        ))}
+        <button
+          className="btn w-full btn-primary mt-auto rounded-full"
+          onClick={handlesuscribtion}
+        >
+          {isSubcrived ? "Card to add" : "Buy now"}
+        </button>
       </div>
-
-      {/* 8 empty divs needed for the 3D effect */}
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-    </a>
+    </div>
   );
 };
 
